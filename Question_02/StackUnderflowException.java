381ffff1913c6b9ad3297beea4640a41fe5a2e36
Vikrant9098 <vikrantnaik4802@gmail.com>
2024-10-22 01:11:22 +0530

    DAY_02
    Question_02 - Stack as ADT(Abstract Data Type)

---
+++ Question_02/StackUnderflowException.java
@@ -0,0 +1,11 @@
+package Classwork;
+
+//Exception for stack underflow
+public class StackUnderflowException extends Exception
+{
+	public StackUnderflowException(String message)
+	{
+		super(message);
+	}
+
+}
